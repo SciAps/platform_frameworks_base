31bdc05d88ac4d52ae2c580fdcac5e2a336b68cc
Shitiz Kumar <shitiz_kumar@ti.com>
2012-07-18 16:19:50 -0500

    FM Rx: Added FM Radio support in Android Framework
    Added FM Radio support in Android Framework. FM Radio device
    is invoked by sending intent to Audio Service by FM Service
    once FM is launched.
    Patch :16536 opens the capture and playback path for FM
    and informs Audio Flinger to stop playback path going into
    standby. Once the FM is turned off the playback handle is
    released
    
    Porting patch from ics-mr0 to ics-mr1
    Porting patch from ICS to JB
    
    Change-Id: I3e9d426a211f2cf49493770f14d694ebe64735e2
    Signed-off-by: Sergii Iegorov <x0155539@ti.com>

--- media/java/android/media/AudioSystem.java
+++ media/java/android/media/AudioSystem.java
@@ -262,6 +262,7 @@ public class AudioSystem
     public static final int DEVICE_IN_DGTL_DOCK_HEADSET = DEVICE_BIT_IN | 0x400;
     public static final int DEVICE_IN_USB_ACCESSORY = DEVICE_BIT_IN | 0x800;
     public static final int DEVICE_IN_USB_DEVICE = DEVICE_BIT_IN | 0x1000;
+    public static final int DEVICE_IN_FM_RADIO_RX = DEVICE_BIT_IN | 0x2000000;
     public static final int DEVICE_IN_DEFAULT = DEVICE_BIT_IN | DEVICE_BIT_DEFAULT;
 
     public static final int DEVICE_IN_ALL = (DEVICE_IN_COMMUNICATION |
@@ -277,6 +278,7 @@ public class AudioSystem
                                              DEVICE_IN_DGTL_DOCK_HEADSET |
                                              DEVICE_IN_USB_ACCESSORY |
                                              DEVICE_IN_USB_DEVICE |
+                                             DEVICE_IN_FM_RADIO_RX |
                                              DEVICE_IN_DEFAULT);
     public static final int DEVICE_IN_ALL_SCO = DEVICE_IN_BLUETOOTH_SCO_HEADSET;
 
